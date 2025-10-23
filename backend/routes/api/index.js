dbe29b7e90d8785bdce9d992bf4c145347d5ef26
Austin Slater <aslater@waldoch.com>
2025-10-23 12:32:48 -0500

    to use new api route /builder

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import quote from './quote.js';
+import builder from './builder.js'
 
 const router = Router();
 
 router.use('/quote', quote);
+router.use('/builder', builder);
 
 export default router;
