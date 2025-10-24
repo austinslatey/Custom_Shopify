2946e8b56efe6243c7434d4075a4d90d1dd5c1e5
Austin Slater <aslater@waldoch.com>
2025-10-24 12:02:43 -0500

    nsCountry and nsState variables added, need to convert frontend values to pass data to netsuite RESTlet

--- backend/utils/netsuite.js
+++ backend/utils/netsuite.js
@@ -2,6 +2,37 @@ import axios from 'axios';
 import crypto from 'crypto';
 import OAuth from 'oauth-1.0a';
 
+const countryMap = {
+    'United States': 'US',
+    'Canada': 'CA',
+    'Other': ''
+};
+
+const stateMap = {
+    'Alabama': 'AL', 'Alaska': 'AK', 'Arizona': 'AZ', 'Arkansas': 'AR',
+    'California': 'CA', 'Colorado': 'CO', 'Connecticut': 'CT', 'Delaware': 'DE',
+    'District of Columbia': 'DC', 'Florida': 'FL', 'Georgia': 'GA', 'Hawaii': 'HI',
+    'Idaho': 'ID', 'Illinois': 'IL', 'Indiana': 'IN', 'Iowa': 'IA',
+    'Kansas': 'KS', 'Kentucky': 'KY', 'Louisiana': 'LA', 'Maine': 'ME',
+    'Maryland': 'MD', 'Massachusetts': 'MA', 'Michigan': 'MI', 'Minnesota': 'MN',
+    'Mississippi': 'MS', 'Missouri': 'MO', 'Montana': 'MT', 'Nebraska': 'NE',
+    'Nevada': 'NV', 'New Hampshire': 'NH', 'New Jersey': 'NJ',
+    'New Mexico': 'NM', 'New York': 'NY', 'North Carolina': 'NC', 'North Dakota': 'ND',
+    'Ohio': 'OH', 'Oklahoma': 'OK', 'Oregon': 'OR', 'Pennsylvania': 'PA',
+    'Rhode Island': 'RI', 'South Carolina': 'SC', 'South Dakota': 'SD',
+    'Tennessee': 'TN', 'Texas': 'TX', 'Utah': 'UT', 'Vermont': 'VT',
+    'Virginia': 'VA', 'Washington': 'WA', 'West Virginia': 'WV',
+    'Wisconsin': 'WI', 'Wyoming': 'WY',
+    'Alberta': 'AB', 'British Columbia': 'BC', 'Manitoba': 'MB', 'New Brunswick': 'NB',
+    'Newfoundland and Labrador': 'NL', 'Nova Scotia': 'NS', 'Ontario': 'ON',
+    'Prince Edward Island': 'PE', 'Quebec': 'QC', 'Saskatchewan': 'SK',
+    'Northwest Territories': 'NT', 'Nunavut': 'NU', 'Yukon': 'YT'
+};
+
+const nsCountry = countryMap[country] || country;
+const nsState = stateMap[state] || state;
+
+
 const netsuiteRequest = async (data) => {
     let url; 
      if (data.isTopperQuote) {
@@ -47,4 +78,5 @@ const netsuiteRequest = async (data) => {
     }
 };
 
-export { netsuiteRequest };
+
+export { netsuiteRequest, nsCountry, nsState };
