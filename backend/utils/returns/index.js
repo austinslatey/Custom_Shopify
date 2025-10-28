c9dc0f75fc1f0a1dfcda3ae340b3e0ea1d63dc9c
Austin Slater <aslater@waldoch.com>
2025-10-28 13:39:12 -0500

    updated email export to the correct file name

--- backend/utils/returns/index.js
+++ backend/utils/returns/index.js
@@ -1,3 +1,3 @@
-export { sendReturnEmails } from './emailAPI.js';
+export { sendReturnEmails } from './email.js';
 export {validateLookupInput , validateSubmitInput, sendValidationError} from './validation.js'
 export { lookupCustomerOrders, processReturnSubmission } from './shopify.js'
