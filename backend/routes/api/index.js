d287adb1725e5490f0f76cd0c62dbf13ea2eb304
Austin Slater <aslater@waldoch.com>
2025-10-28 10:55:24 -0500

    new route added /returns

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import quote from './quote.js';
 import builder from './builder.js'
+import returns from './returns.js'
 
 const router = Router();
 
 router.use('/quote', quote);
 router.use('/builder', builder);
+router.use('/returns', returns);
 
 export default router;
