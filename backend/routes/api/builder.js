e329c169cf96cbba38d0c4c210cfe58b92cf51d8
Austin Slater <aslater@waldoch.com>
2025-10-28 11:11:19 -0500

    Barreled builder utils instead of importing each module directly

--- backend/routes/api/builder.js
+++ backend/routes/api/builder.js
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 import fs from 'fs/promises';
-import { validateVehicleConfigRequest, sanitizeVehicleConfigData } from '../../utils/builder/vehicleValidation.js';
-import { submitToVehicleConfigHubspot } from '../../utils/builder/vehicleConfigHubSpot.js';
-import { sendVehicleConfigEmail } from '../../utils/builder/sendVehicleEmail.js';
+import { validateVehicleConfigRequest, sanitizeVehicleConfigData, submitToVehicleConfigHubspot, sendVehicleConfigEmail} from "../../utils/builder/index.js"
 import { netsuiteRequest, nsCountry, nsState } from '../../utils/netsuite.js';
 import { vehicleConfigUpload } from '../../middleware/multer.js';
 
